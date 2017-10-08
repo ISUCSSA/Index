416a62f47205e06dd6fa2a15fd7880fbe8e24d2f
WMXPY <wm@mengw.io>
2017-10-08 18:10:38 -0500

    update dogui version to lastest, move contact outside, done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <DogUIISUCSSA tabs={this.tabs}>DEPARTMENT OF CAREER DEVELOPMENT</DogUIISUCSSA>
+        <DogUIISUCSSA
+          tabs={this.tabs}
+          source={{ name: "ISUCSSA", link: "https://github.com/ISUCSSA/Index" }}
+          contact={{
+            email: "cssa.isu.ia@gmail.com",
+            address: "2229 Lincoln Way C4 1530 Student Office",
+            address2: "Memorial Union Ames, iA 50011-1130",
+            wechat: "http://www.isucssa.org/24494204493902938754.html",
+            weibo: "http://weibo.com/isucssa",
+            facebook: "https://www.facebook.com/iaisucssa",
+            instagram: "https://www.instagram.com/isucssa/"
+          }}
+        >
+          DEPARTMENT OF CAREER DEVELOPMENT
+        </DogUIISUCSSA>
       </div>
     );
   }
