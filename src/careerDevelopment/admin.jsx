60721a754a7a0841e9e5a64976e51f94b9e31b57
WMXPY <wm@mengw.io>
2017-10-20 11:36:50 -0500

    template

--- src/careerDevelopment/admin.jsx
+++ src/careerDevelopment/admin.jsx
@@ -5,8 +5,27 @@ class Admin extends Component {
     render() {
         return (
             <div>
-                名字<input></input>
-                内容<textarea></textarea>
+                <div>
+                    <h3>链接一篇微信文章</h3>
+                    <label>你要连接的微信文章地址</label><input></input>
+                    <br />
+                    <label>这篇文章应该显示的标题</label><input></input>
+                    <hr />
+                </div>
+                <div>
+                    <h3>展示一份工作</h3>
+                    <label>你要连接的微信文章地址</label><input></input>
+                    <br />
+                    <label>这篇文章应该显示的标题</label><input></input>
+                    <hr />
+                </div>
+                <div>
+                    <h3>建立一次带报名和投票的活动</h3>
+                    <label>你要连接的微信文章地址</label><input></input>
+                    <br />
+                    <label>这篇文章应该显示的标题</label><input></input>
+                    <hr />
+                </div>
             </div>
         );
     }
