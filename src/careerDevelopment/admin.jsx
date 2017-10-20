6e9ba1f66d89631148b67c1550e6d974880c8c8b
WMXPY <wm@mengw.io>
2017-10-20 11:41:15 -0500

    website

--- src/careerDevelopment/admin.jsx
+++ src/careerDevelopment/admin.jsx
@@ -21,10 +21,10 @@ class Admin extends Component {
                 </div>
                 <div>
                     <h3>建立一次带报名和投票的活动</h3>
-                    <label>你要连接的微信文章地址</label><input></input>
+                    <label>这次活动的名字</label><input></input>
                     <br />
-                    <label>这篇文章应该显示的标题</label><input></input>
-                    <hr />
+                    <label>活动的说明 (请将你的文章在任意 HTML 编辑网站编辑, 并复制到此处, 比如<a href="http://html-online.com/editor/">这个网站</a>)</label>
+                    <textarea></textarea>
                 </div>
             </div>
         );
