220bf8ffd52da61093eeee9c3a8b47b8f91221b3
WMXPY <wm@mengw.io>
2017-10-25 18:06:04 -0500

    tech page by halibuts

--- src/tech/index.jsx
+++ src/tech/index.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import Halibut from 'halibuts';
+import Halibut, { WEATHERS } from 'halibuts';
 
 class ERROR404 extends Component {
     render() {
-
+        console.log(WEATHERS)
         return (
             <div style={{ textAlign: "center" }}>
-                <Halibut weather="Hailing">
+                <Halibut weather="Windy">
                     <div style={{
                         backgroundColor: "rgba(255, 255, 255, 0.212)",
                         height: "100%",
                         width: "100%"
                     }}>
-                        <h1>:D</h1>
+                        <h1 style={{ fontSize: "75px", color: "white", fontWeight: "bold", paddingTop: "6%" }}>Hello World!</h1>
+                        <h2 style={{ fontSize: "30px", color: "white", fontWeight: "bold", paddingTop: "2%" }}>(Just me)</h2>
                     </div>
                 </Halibut>
             </div>
