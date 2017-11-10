c33d4ee923b689f2692174e3758931691043ce84
WMXPY <wm@mengw.io>
2017-11-10 16:12:48 -0600

    init

---
+++ src/alewife/src/video.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ERROR404 extends Component {
+    render() {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <h2>404 Nothing Found</h2>
+            </div>
+        );
+    }
+}
+
+export default ERROR404;
