65c9790222695e6fbd42ba6ca00986ddd512650f
WMXPY <wm@mengw.io>
2017-11-11 23:50:54 -0600

    banner

--- src/index/banner.jsx
+++ src/index/banner.jsx
@@ -18,18 +18,14 @@ class IndexBanner extends Component {
                     <div style={{ fontSize: "3vw", color: "white", paddingTop: "15%" }} >
                         <span style={{ paddingLeft: "3vw", fontSize: "2vw" }}>查看CSSA各个部门的最新进展 / Lookup latest upcoming of CSSA</span>
                         <div className="row">
-                            <div className="col-sm-4">
-                                <button className="bannerButton" onClick={() => window.location.href = './career'}>Career / 职业发展部</button>
+                            <div className="col-sm-4 col-sm-offset-2">
+                                <button className="bannerButton" onClick={() => window.location.href = '/departments'}>Departments / 部门列表</button>
                             </div>
                             <div className="col-sm-4">
-                                <button className="bannerButton" onClick={() => window.location.href = './tech'}>Tech / 技术部</button>
-                            </div>
-                            <div className="col-sm-4">
-                                <button className="bannerButton" onClick={() => window.location.href = './example'}>Example / 例子部</button>
+                                <button className="bannerButton" onClick={() => window.location.href = '/contect'}>Contect / 联系我们</button>
                             </div>
                         </div>
                     </div>
-
                 </Video>
                 <div style={{ height: "0.3vh", backgroundColor: "rgb(241, 190, 72)", marginBottom: "1vh" }} />
                 <div style={{ textAlign: "center" }}>
